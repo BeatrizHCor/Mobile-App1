886f4ea9abbe21d5bd32e552cb2e3baf7a220e36
fumireko <fumireko@yandex.com>
2024-10-11 19:00:29 -0300

    adicionar lógica no MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,72 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.util.Random
+
 
 class MainActivity : AppCompatActivity() {
+    var num1 = 0
+    var num2 = 0
+    var resposta = 0
+    var score = 0
+    var perguntaAtual = 1
+
+    lateinit var perguntaTextView: TextView
+    lateinit var scoreTextView: TextView
+    lateinit var respostaEditText: EditText
+    lateinit var botao: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        botao = findViewById<Button>(R.id.submit)
+        perguntaTextView = findViewById<TextView>(R.id.pergunta)
+        scoreTextView = findViewById<TextView>(R.id.score)
+        respostaEditText = findViewById<EditText>(R.id.resposta)
+    }
+
+    private fun gerarPergunta() {
+        val random: Random = Random()
+        num1 = random.nextInt(100)
+        num2 = random.nextInt(100)
+        resposta = num1 + num2
+        perguntaTextView!!.text = "$num1 + $num2 = ?"
+    }
+
+    private fun checarResposta() {
+        val respostaUsuario = respostaEditText!!.text.toString()
+        if (respostaUsuario.isEmpty()) {
+            Toast.makeText(this, "Digite uma resposta", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (respostaUsuario.toInt() == resposta) {
+            score += 20
+            //findViewById<View>(R.id.pergunta).setBackgroundResource(Color.GREEN)
+        } else {
+            //findViewById<View>(R.id.pergunta).setBackgroundResource(Color.RED)
+            Toast.makeText(this, "Resposta correta: $resposta", Toast.LENGTH_SHORT).show()
+        }
+
+        if (perguntaAtual == 5) {
+            perguntaTextView!!.visibility = View.GONE
+            respostaEditText!!.visibility = View.GONE
+            botao!!.visibility = View.GONE
+
+            scoreTextView!!.visibility = View.VISIBLE
+            scoreTextView!!.text = "Sua pontuação: $score/100"
+        } else {
+            perguntaAtual++
+            gerarPergunta()
+            respostaEditText!!.setText("")
         }
     }
 }
